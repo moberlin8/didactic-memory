1a73a1812ae371e4a102380a49d39d652581023b
Matt Oberlin <mao@us.ibm.com>
2019-12-01 08:13:08 -0800

    Simple mod

--- src/main/java/com/ibm/hello/config/HelloConfig.java
+++ src/main/java/com/ibm/hello/config/HelloConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "hello")
+@ConfigurationProperties(prefix = "Hola")
 public class HelloConfig {
     private String pattern;
 
